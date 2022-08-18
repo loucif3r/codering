5f58b5841b265045ec947bae92ffa30b7adf9a15
loucif3r <111393655+loucif3r@users.noreply.github.com>
2022-08-18 18:34:36 +0430

    Add files via upload

---
+++ coder.py
@@ -0,0 +1,96 @@
+from colorama import Fore
+import sys
+import os
+from time import sleep
+import time
+
+os.system ("clear")
+
+print (Fore.BLUE+"\n[M]")
+sleep(0.07)
+print ("\n[R]")
+sleep(0.07)
+print ("\n[B]")
+sleep(0.07)
+print ("\n[L]")
+sleep(0.07)
+print ("\n[A]")
+sleep(0.07)
+print ("\n[C]")
+sleep(0.07)
+print ("\n[K]\n")
+sleep(0.07)
+
+print (Fore.GREEN)
+kop = """ 
+
+                    Mr weblog Or Mr Black              
+
+"""
+for c in kop:
+    sys.stdout.write(c)
+    sys.stdout.flush()
+    time.sleep(0.05)
+
+print (Fore.RED+"""   ______          __
+  / ____/___  ____/ /__  _____
+ / /   / __ \/ __  / _ \/ ___/
+/ /___/ /_/ / /_/ /  __/ /
+\____/\____/\__,_/\___/_/
+\n""")
+
+print (Fore.WHITE+"Phone number"+" . "+Fore.YELLOW+"09929115957\n"+Fore.WHITE+"Lockader:"+Fore.YELLOW+"Mr . Black or Mr . Weblog")
+
+print ("""
+               code.hangi= 1
+               code.filteri= 2
+               link.mokhareb= 3
+               exit= 0""")
+              
+
+print (Fore.RED)
+
+x = input("enter the number==> ")
+sleep(0.5)
+print (Fore.WHITE)
+print ("....")
+sleep(0.5)
+print (Fore.RED)
+print (".........")
+sleep(0.5)
+print (Fore.YELLOW)
+print ("..............")
+sleep(0.5)
+print (Fore.BLUE)
+print ("......................")
+sleep(0.5)
+print (Fore.GREEN)
+print ("...............................")
+sleep(0.5)
+
+print (Fore.RED)
+
+coder = '1'
+coder_2 = '2'
+coder_3 = '3'
+coder_0 = '0'
+if x == coder:
+    print (Fore.YELLOW+"CODE HANGI\n")
+    print (Fore.RED+"""[1]\n1.2.3.1.2.3.1.2.3.1.2.3.1.2.3.1.2.3.1.2.3.1.2.3.1.2.3.1.2.3.1.2.3.1.2.3.1.2.3.1.2.3.1.2.3.1.2.3.1.2.3.1.2.3.1.2.3.1.2.3.1.2.3.1.2.3.1.2.3.1.2.3.1.2.3.1.2.3.1.2.3.1.2.3.1.2.3.1.2.3.1.2.3.1.2.3.1.2.3.1.2.3.1.2.3.1.2.3.1.2.3.1.2.3.1.2.3.1.2.3.1.2.3.1.2.3.1.2.3""")
+if x == coder_2:
+    print (Fore.YELLOW+"CODE FILTERI\n")
+    print (Fore.RED+"""[1]    https://rubika.ir.Filter.filterrubika.https://bit.ly/3fV728378l.16:45:09.158.filtering.rubika.sex.gif.https://s6.uupload.ir/files/img_20220421_122708_504_zx3o.jpg.rubika.sex.in.photo.sex
+    
+[2]    Dxprit.erorr18.filtering.spam.server.erorr.yftt.com.https://bit.ly/3fX8ljZ.19 20:38:10.053.http://Dxprit.ngopb.hackers65.78098755434343434766.infect.in.https://s4.uupload.ir/files/screenshot_۲۰۲۱۱۰۰۸-۲۳۰۵۰۳_l5zo.png.http://Dxprit.ngopb.hackers65.780999999998755766.infect.in.sxs.photo.in.rubika""")
+if x == coder_3:
+    print (Fore.YELLOW+"LINK MOKHAREB\n")
+    print (Fore.RED+"""[1]    https://bit.ly/3fV728378l
+
+[2]    https://bit.ly/3ild93Ljz
+
+[3]    https://bit.ly/3ild93L
+
+[4]    https://bit.ly/3ild93L""")
+
+while x != coder_0:
+    x = input("\nTHE END")
